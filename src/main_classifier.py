aa77420c74b22d7e53d52ef81c8715e193efc37e
urosjarc <jar.fmf@gmail.com>
2021-01-17 01:50:04 +0100

    Now drawing testing surface and adding points.

--- src/main_classifier.py
+++ src/main_classifier.py
@@ -4,51 +4,46 @@ from PyQt5.QtCore import QTimer
 from pyqtgraph.Qt import QtGui
 
 from src.math import *
+from pyqtgraph.examples import run
+run()
 
 class Plot:
-    def __init__(self):
+
+    def __init__(self, surface: Surface):
+        self.fun: Surface = surface
         self.app = QtGui.QApplication([])
 
         self.grid = gl.GLGridItem()
         self.grid.scale(1, 1, 1)
         self.grid.setDepthValue(10)
 
-        self.surface = gl.GLSurfacePlotItem(smooth=True, shader='heightColor', glOptions='opaque')
+        self.surface = gl.GLSurfacePlotItem(x=self.fun.x, y=self.fun.y, z=self.fun.z, smooth=True, shader='viewNormalColor', glOptions='opaque')
         self.points = gl.GLScatterPlotItem()
         self.points._pos = []
 
         self.win = gl.GLViewWidget()
         self.win.setWindowTitle('pyqtgraph example: GLSurfacePlot')
-        self.win.setCameraPosition(distance=50,elevation=90, azimuth=0)
+        self.win.setCameraPosition(distance=200,elevation=90, azimuth=0)
 
         self.win.addItem(self.grid)
         self.win.addItem(self.surface)
         self.win.addItem(self.points)
 
         self.timer = QTimer()
-        self.timer.timeout.connect(self.update)
-
-        self.init()
+        self.timer.timeout.connect(self.addPoint)
 
     def init(self):
-        self.draw()
         self.win.show()
         self.timer.start(0.5)
+        self.app.exec_()
 
-    def draw(self):
-        x, y, z = surface(fun.rastrigin)
-        self.surface.setData(x, y, z)
-
-    def addPoint(self, pos):
+    def addPoint(self):
+        pos = [randrange(*self.fun.range) for i in range(2)]
+        pos.append(self.fun(pos[0], pos[1]))
         self.points._pos.append(pos)
         self.points.setData(pos=np.array(self.points._pos))
 
-    def run(self):
-        self.app.exec_()
-
-    def update(self):
-        point = [randrange(-10, 10) for i in range(2)] + [0]
-        self.addPoint(point)
-
-plot = Plot()
-plot.run()
+surface = Surface(fun.rastrigin, range=(-5, 5))
+# surface = Surface(fun.eggholder, range=(-1000, 1000))
+plot = Plot(surface)
+plot.init()
--- src/math.py
+++ src/math.py
@@ -1,22 +1,29 @@
 import numpy as np
 from typing import Callable
 
-class fun:
-    rastrigin = lambda x, y: ((x**2 - 10 * np.cos(2 * np.pi * x)) + (y**2 - 10 * np.cos(2 * np.pi * y)) + 20)
-    eggholder = lambda x, y: (-(y + 47) * np.sin(np.sqrt(abs(x / 2 + (y + 47)))) - x * np.sin(np.sqrt(abs(x - (y + 47)))))
 
-def normalize_surface(matrix, mini=-1, height=2):
-    matrix -= np.min(matrix)
-    matrix /= np.max(matrix)
-    matrix *= height
-    matrix += mini
+class fun:
+    rastrigin = lambda x, y: ((x ** 2 - 10 * np.cos(2 * np.pi * x)) + (y ** 2 - 10 * np.cos(2 * np.pi * y)) + 20)
+    eggholder = lambda x, y: (
+            -(y + 47) * np.sin(np.sqrt(abs(x / 2 + (y + 47)))) - x * np.sin(np.sqrt(abs(x - (y + 47)))))
+
+
+def normalize(matrix, minV, maxV):
+    ave = (minV + maxV) / 2
+    dy = maxV - ave
+    matrix -= ave
+    matrix /= dy
     return matrix
 
-def surface(f: Callable, xmM=(-10, 10), ymM=(-10, 10), step=400):
-    x = np.linspace(xmM[0], xmM[1], step)
-    y = np.linspace(ymM[0], ymM[1], step)
-    xx, yy = np.meshgrid(x, y, sparse=True)
-    z = normalize_surface(f(xx, yy))
-    return (x, y, z)
 
+class Surface:
+    def __init__(self, f: Callable, range=(-10, 10), step=400):
+        self.range = range
+        self.f = f
+        self.x = np.linspace(range[0], range[1], step)
+        self.y = np.linspace(range[0], range[1], step)
+        xx, yy = np.meshgrid(self.x, self.y, sparse=True)
+        self.z = self.f(xx, yy)
 
+    def __call__(self, x, y, norm=True):
+        return self.f(x, y)
