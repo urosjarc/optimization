9314e264b40e03da574a4d4cd2f6f6e07584060a
urosjarc <jar.fmf@gmail.com>
2021-01-17 03:05:57 +0100

    Now drawing testing surface and adding points.

--- src/main_classifier.py
+++ src/main_classifier.py
@@ -1,49 +1,44 @@
-from random import randrange
-import pyqtgraph.opengl as gl
-from PyQt5.QtCore import QTimer
-from pyqtgraph.Qt import QtGui
+import random
+
+import matplotlib.pyplot as plt
 
 from src.math import *
-from pyqtgraph.examples import run
-run()
+
+
+# run()
 
 class Plot:
 
     def __init__(self, surface: Surface):
         self.fun: Surface = surface
-        self.app = QtGui.QApplication([])
-
-        self.grid = gl.GLGridItem()
-        self.grid.scale(1, 1, 1)
-        self.grid.setDepthValue(10)
-
-        self.surface = gl.GLSurfacePlotItem(x=self.fun.x, y=self.fun.y, z=self.fun.z, smooth=True, shader='viewNormalColor', glOptions='opaque')
-        self.points = gl.GLScatterPlotItem()
-        self.points._pos = []
-
-        self.win = gl.GLViewWidget()
-        self.win.setWindowTitle('pyqtgraph example: GLSurfacePlot')
-        self.win.setCameraPosition(distance=200,elevation=90, azimuth=0)
-
-        self.win.addItem(self.grid)
-        self.win.addItem(self.surface)
-        self.win.addItem(self.points)
-
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.addPoint)
+        self.fig = None
+        self.scatter = None
+        self.init()
 
     def init(self):
-        self.win.show()
-        self.timer.start(0.5)
-        self.app.exec_()
+        self.fun.init()
+        plt.ion()
+        self.fig, ax = plt.subplots(1, 1)
+        cp = ax.contourf(self.fun.x, self.fun.y, self.fun.z, levels=100, cmap="seismic")
+        self.fig.colorbar(cp)
 
-    def addPoint(self):
-        pos = [randrange(*self.fun.range) for i in range(2)]
-        pos.append(self.fun(pos[0], pos[1]))
-        self.points._pos.append(pos)
-        self.points.setData(pos=np.array(self.points._pos))
+        plt.scatter([self.fun.min[0]], [self.fun.min[1]], color='w')
 
-surface = Surface(fun.rastrigin, range=(-5, 5))
-# surface = Surface(fun.eggholder, range=(-1000, 1000))
-plot = Plot(surface)
+        self.scatter = ax.scatter([0], [0], color='y')
+        self.scatter._x = []
+        self.scatter._y = []
+
+        plt.show()
+
+    def addPoint(self, x, y):
+        self.scatter._x.append(x)
+        self.scatter._y.append(y)
+        self.scatter.set_offsets(np.c_[self.scatter._x, self.scatter._y])
+        self.fig.canvas.draw_idle()
+
+
+plot = Plot(fun.rastigin)
+for i in range(1000):
+    plot.addPoint(random.random()*100, random.random()*100)
+    plt.pause(0.1)
 plot.init()
--- src/math.py
+++ src/math.py
@@ -2,28 +2,30 @@ import numpy as np
 from typing import Callable
 
 
-class fun:
-    rastrigin = lambda x, y: ((x ** 2 - 10 * np.cos(2 * np.pi * x)) + (y ** 2 - 10 * np.cos(2 * np.pi * y)) + 20)
-    eggholder = lambda x, y: (
-            -(y + 47) * np.sin(np.sqrt(abs(x / 2 + (y + 47)))) - x * np.sin(np.sqrt(abs(x - (y + 47)))))
-
-
-def normalize(matrix, minV, maxV):
-    ave = (minV + maxV) / 2
-    dy = maxV - ave
-    matrix -= ave
-    matrix /= dy
-    return matrix
-
-
 class Surface:
-    def __init__(self, f: Callable, range=(-10, 10), step=400):
+
+    def __init__(self, f: Callable, min, range, step=400):
+        self.min = min
         self.range = range
         self.f = f
-        self.x = np.linspace(range[0], range[1], step)
-        self.y = np.linspace(range[0], range[1], step)
+        self.step = step
+        self.x = []
+        self.y = []
+        self.z = []
+
+    def init(self):
+        self.x = np.linspace(self.range[0], self.range[1], self.step)
+        self.y = np.linspace(self.range[0], self.range[1], self.step)
         xx, yy = np.meshgrid(self.x, self.y, sparse=True)
         self.z = self.f(xx, yy)
 
     def __call__(self, x, y, norm=True):
         return self.f(x, y)
+
+class fun:
+    _rastrigin = lambda x, y: ((x ** 2 - 10 * np.cos(2 * np.pi * x)) + (y ** 2 - 10 * np.cos(2 * np.pi * y)) + 20)
+    _eggholder = lambda x, y: (
+            -(y + 47) * np.sin(np.sqrt(abs(x / 2 + (y + 47)))) - x * np.sin(np.sqrt(abs(x - (y + 47)))))
+
+    rastigin = Surface(_rastrigin, min=(0, 0, 0), range=(-5.12, 5.12))
+    eggholder = Surface(_eggholder, min=(512, 404.2319, -959.6407), range=(-512, 512))
