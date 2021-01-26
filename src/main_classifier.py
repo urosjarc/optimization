c5df0560352ec17d0beafb0c3312178072c2596d
urosjarc <jar.fmf@gmail.com>
2021-01-27 00:40:48 +0100

    Finishing plotting.

--- src/main_classifier.py
+++ src/main_classifier.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
-import numpy as np
+from matplotlib import gridspec
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Path3DCollection
+
 from src.math import *
-from gobench import go_benchmark_functions
+
+
 
 
 class Plot:
@@ -9,45 +13,47 @@ class Plot:
         self.surface: Surface = Surface(function)
         self.scatter = None
         self.scatterLog = None
+        self.scatter3D = None
         self.step = 51
         self.fig = None
-
+        self.points = [[],[],[]]
         self.show()
 
     def show(self):
+        zLog = lambda z: np.log2(z - self.surface.min + 1)
+        opt = self.surface.opt
+
         # Creating matrix xx, yy, zz
         x = np.linspace(self.surface.f.bounds[0][0], self.surface.f.bounds[0][1], self.step)
         y = np.linspace(self.surface.f.bounds[1][0], self.surface.f.bounds[1][1], self.step)
         xx, yy = np.meshgrid(x, y, sparse=True)
         zz = np.array([[self.surface(xi, yi) for xi in x] for yi in y])
-        zzLog = np.log2(zz - self.surface.min + 1)
+        zzLog = zLog(zz)
 
         # Creating points setup
-        opt = self.surface.opt
         zMin = np.array([self.surface.min for _ in opt])
         xMin = [o[0] for o in opt]
         yMin = [o[1] for o in opt]
-        zMinLog = np.log2(zMin - self.surface.min + 1)
+        zMinLog = zLog(zMin)
 
         # Drawing main figure
         plt.ion()
+        gs = gridspec.GridSpec(2, 2)
         fig = plt.figure()
         self.fig = fig
         fig.suptitle(f'{self.surface.name} - {len(self.surface.opt)} min.')
 
         # Drawing contour bars normal
-        ax = fig.add_subplot(2, 2, 2)
+        ax = fig.add_subplot(gs[0,1])
         cp = ax.contourf(x, y, zz, levels=self.step, cmap="jet")
         ax.scatter(xMin, yMin, marker='*', color='red')
         fig.colorbar(cp)
 
         # Drawing points
         self.scatter = ax.scatter([], [], marker='x', color='red')
-        self.scatter._x = []
-        self.scatter._y = []
 
         # Drawing contour bars log
-        ax = fig.add_subplot(2, 2, 4)
+        ax = fig.add_subplot(gs[1,1])
         cp = ax.contourf(x, y, zzLog, levels=self.step, cmap="jet")
         ax.scatter(xMin, yMin, marker = '*', color='red')
         fig.colorbar(cp)
@@ -56,30 +62,25 @@ class Plot:
         self.scatterLog = ax.scatter([], [], marker='x', color='red')
 
         # Drawing surface 3D
-        ax = fig.add_subplot(2, 2, 1, projection='3d')
-        ax.plot_surface(xx, yy, zz,cmap="jet", linewidth=0, antialiased=True)
-        ax.scatter(xMin, yMin, zMin, color='red')
-        ax = fig.add_subplot(2, 2, 3, projection='3d')
-        ax.scatter(xMin, yMin, zMinLog, color='red')
-        ax.plot_surface(xx, yy, zzLog, cmap="jet", linewidth=0, antialiased=True)
+        ax = fig.add_subplot(gs[:,0], projection='3d')
+        ax.plot_surface(xx, yy, zz, alpha=0.5, cmap="jet", linewidth=0, antialiased=True)
+        self.scatter3D = ax.scatter([],[],[], 'o', color='black')
         plt.show()
 
     def addPoint(self, x, y):
-        self.scatter._x.append(x)
-        self.scatter._y.append(y)
-        self.scatter.set_offsets(np.c_[self.scatter._x, self.scatter._y])
-        self.scatterLog.set_offsets(np.c_[self.scatter._x, self.scatter._y])
+        self.points[0].append(x)
+        self.points[1].append(y)
+        self.points[2].append(self.surface(x, y))
+        self.scatter.set_offsets(np.c_[self.points[0], self.points[1]])
+        self.scatterLog.set_offsets(np.c_[self.points[0], self.points[1]])
+        self.scatter3D._offsets3d = self.points
         self.fig.canvas.draw_idle()
 
 funs:List[Benchmark] = functions()
 for i, f in enumerate(funs):
     if dimensions(f) == 2:
         plot = Plot(f)
-        for i in range(10):
-            import random
-            plot.addPoint(random.random()*10 - 5,random.random()*10 - 5)
-        print(i, plot.surface.name)
         plt.savefig(f'../data/functions/{i:03d}_{plot.surface.name}.png')
-        # plt.close()
-
         plt.waitforbuttonpress()
+        plt.close()
+
