18b07b84d2e676f85649f38bc06054ecf8d23be2
urosjarc <jar.fmf@gmail.com>
2021-02-08 22:07:36 +0100

    Shit is working now!!!

--- src/optimization/triangle.py
+++ src/optimization/triangle.py
@@ -12,11 +12,12 @@ from typing import *
 class Point:
     def __init__(self, x, y, value):
         self.vector = [x, y]
-        self.vector3D = [x, y, value]
+        self.vector3D = np.array([x, y, value])
         self.x = x
         self.y = y
         self.value = value
         self.triangles = []
+        self.lines = []
 
     def __str__(self):
         return f'({self.x},{self.y},{self.value})'
@@ -25,29 +26,23 @@ class Point:
         if onSurface:
             return np.linalg.norm(np.array(self.vector) - np.array(point.vector))
         else:
-            return np.linalg.norm(np.array(self.vector3D) - np.array(point.vector3D))
+            return np.linalg.norm(self.vector3D - point.vector3D)
 
 
 class Line:
     def __init__(self, p1: Point, p2: Point):
         self.points = [p1, p2]
-
-        self.size = None
-        self.center = None
-
-
-        self.init()
-
-    def init(self):
-        # Size
-        self.size = self.points[0].distance(self.points[1], onSurface=False)
-
-        # Center
-        self.center = [0 for _ in range(3)]
         for p in self.points:
-            for i, ax in enumerate(p.vector3D):
-                self.center[i] += ax
-        self.center = [ax/2 for ax in self.center]
+            p.lines.append(self)
+
+    def size(self, onSurface):
+        return self.points[0].distance(self.points[1], onSurface)
+
+    def center(self, onSurface):
+        if onSurface:
+            return np.mean([p.vector for p in self.points], axis=0).tolist()
+        else:
+            return np.mean([p.vector3D for p in self.points], axis=0).tolist()
 
     def mutualPoint(self, line):
         p1 = set(self.points)
@@ -57,7 +52,7 @@ class Line:
     def isOnLine(self, point: Point, onSurface):
         dist = self.points[0].distance(point, onSurface)
         dist += self.points[1].distance(point, onSurface)
-        return abs(dist - self.size) < 10 ** -7
+        return abs(dist - self.size(onSurface)) < 10 ** -7
 
     def coinciding(self, line, onSurface):
         p1, p2 = line.points
@@ -70,24 +65,11 @@ class Line:
 class Triangle:
     def __init__(self, lines, evalDiff=10 ** 10, eval=0):
         self.lines: List[Line] = lines
-        self.points: List[Point] = []
-
         self.evalDiff: float = abs(evalDiff)
         self.eval = eval
+        self.points: List[Point] = []
 
-        self.meanValue = None
-        self.lowestPoint: Point = None
-        self.normal = None
-        self.volume = None
-        self.nextPointVector = None
-
-        self.init()
-
-    def init(self):
-        # Biggest line sort
-        self.lines.sort(key=lambda l: l.size, reverse=True)
-
-        # Get triangle points *3
+        # Get unique points
         pts = set()
         for l in self.lines:
             for p in l.points:
@@ -98,40 +80,42 @@ class Triangle:
         for p in self.points:
             p.triangles.append(self)
 
-        # Mean value
+    def meanValue(self):
         vals = [p.value for p in self.points]
-        self.meanValue = sum(vals) / 3
+        return sum(vals) / 3
 
-        # Lowest point
-        self.lowestPoint = sorted(self.points, key=lambda p: p.value, reverse=False)[0]
+    def lowestPoint(self):
+        return sorted(self.points, key=lambda p: p.value, reverse=False)[0]
 
-        # Normal vector
-        v1 = np.array(self.points[0].vector3D)
-        v2 = np.array(self.points[1].vector3D)
-        v3 = np.array(self.points[2].vector3D)
+    def normalVector(self):
+        """Returns normal vector which is turned upward"""
+        v1 = self.points[0].vector3D
+        v2 = self.points[1].vector3D
+        v3 = self.points[2].vector3D
         dv1 = v2 - v1
         dv2 = v3 - v1
         normal1 = np.cross(dv1, dv2)
         if normal1[-1] < 0:
             normal1 = np.cross(dv2, dv1)
-        self.normal = normal1 / 2
+        return normal1 / 2
 
-        # Slope ratio
+    def slopeRatio(self):
+        normal = self.normalVector()
         vY = np.array([0, 0, 1])
-        self.slopeRatio = angle(self.normal, vY) / (math.pi/2)
-
-        # Volume
-        A, B, C = self.points
-        self.volume = abs((A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y)) / 2)
-
-        # Next point vector
-        self.nextPointVector = self.lines[0].center
+        return angle(normal, vY) / (math.pi/2)
 
     def fractureRatio(self):
         deg = []
         for tri in self.connectedTriangles(onSurface=False):
-            deg.append(angle(self.normal, tri.normal))
+            deg.append(angle(self.normalVector(), tri.normalVector()))
         return np.median(deg) / (math.pi/2)
+
+    def volume(self):
+        A, B, C = self.points
+        return abs((A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y)) / 2)
+
+    def sortedLines(self, onSurface, fromBigToLow=True):
+        return sorted(self.lines, key=lambda l: l.size(onSurface), reverse=fromBigToLow)
 
     def connectedTriangles(self, onSurface):
         tri = []
@@ -151,6 +135,9 @@ class Triangle:
 
         return tri
 
+    def newPointVector(self, onSurface):
+        bigestLine = self.sortedLines(onSurface, fromBigToLow=True)[0]
+        return bigestLine.center(onSurface)
 
 
 class TriangleOptimizer:
@@ -223,13 +210,13 @@ class TriangleOptimizer:
             return ts[0]
 
         evalDiff = normalizeVector([t.evalDiff for t in ts])
-        meanValue = 1 - normalizeVector([t.meanValue for t in ts])
-        lovestValue = 1 - normalizeVector([t.lowestPoint.value for t in ts])
-        slopeRatio = normalizeVector([t.slopeRatio for t in ts])
-        volume = normalizeVector([t.volume for t in ts])
-        # fractureRatio = normalizeVector([t.fractureRatio() for t in ts])
+        meanValue = 1 - normalizeVector([t.meanValue() for t in ts])
+        lovestValue = 1 - normalizeVector([t.lowestPoint().value for t in ts])
+        slopeRatio = normalizeVector([t.slopeRatio() for t in ts])
+        volume = normalizeVector([t.volume() for t in ts])
+        fractureRatio = normalizeVector([t.fractureRatio() for t in ts])
 
-        rank = evalDiff + meanValue + lovestValue + slopeRatio + volume #+ #3 * fractureRatio
+        rank = evalDiff + meanValue + lovestValue + slopeRatio + volume + 3 * fractureRatio
         higestRank = np.argsort(rank)[-1]
         return ts[higestRank]
 
@@ -245,8 +232,8 @@ class TriangleOptimizer:
     def partition(self, triangle: Triangle):
 
         # Get new point vector of splited triangle line, and create new point from that
-        line0, line1, line2 = triangle.lines
-        mX, mY, mVal = line0.center
+        line0, line1, line2 = triangle.sortedLines(onSurface=True, fromBigToLow=True)
+        mX, mY, mVal = line0.center(onSurface=False)
         mPoint, cmd = self.getOrMakePoint(mX, mY)
         evalDiff = mPoint.value - mVal
 
@@ -278,7 +265,7 @@ class TriangleOptimizer:
         # Get triangles that can be splited without creating new point
         neighbours = []
         for t in triangle.connectedTriangles(onSurface=True):
-            mX, mY, mVal = t.nextPointVector
+            mX, mY = t.newPointVector(onSurface=True)
             if self.pointExists(mX, mY):
                 neighbours.append(t)
 
